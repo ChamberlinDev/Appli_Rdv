09a9136c79f8824bf15965078f087b7af8293b32
ChamberlinDev <himalayaommet@gmail.com>
2024-11-02 23:09:52 +0000

    Sauvegarde77

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,3 +1,60 @@
-<footer class="py-2 bg-dark">
-            <div class="container px-4"><p class="m-0 text-center text-white">Licence GL &copy; Memoire 2024</p></div>
- </footer>
+	<!-- Debut Footer Area -->
+    <footer id="footer" class="footer ">			
+			<!-- Copyright -->
+			<div class="copyright">
+				<div class="container">
+					<div class="row">
+						<div class="col-lg-12 col-md-12 col-12">
+							<div class="copyright-content">
+								<p>© Copyright 2024 |  Memoire Licence GL <a href="https://www.groupeisi.com" target="_blank">groupeisi.com</a> </p>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+			<!--/ End Copyright -->
+		</footer>
+		<!--/ Fin Footer Area -->
+		
+		<!-- jquery Min JS -->
+        <script src="assets/js/jquery.min.js"></script>
+		<!-- jquery Migrate JS -->
+		<script src="assets/js/jquery-migrate-3.0.0.js"></script>
+		<!-- jquery Ui JS -->
+		<script src="assets/js/jquery-ui.min.js"></script>
+		<!-- Easing JS -->
+        <script src="assets/js/easing.js"></script>
+		<!-- Color JS -->
+		<script src="assets/js/colors.js"></script>
+		<!-- Popper JS -->
+		<script src="assets/js/popper.min.js"></script>
+		<!-- Bootstrap Datepicker JS -->
+		<script src="assets/js/bootstrap-datepicker.js"></script>
+		<!-- Jquery Nav JS -->
+        <script src="assets/js/jquery.nav.js"></script>
+		<!-- Slicknav JS -->
+		<script src="assets/js/slicknav.min.js"></script>
+		<!-- ScrollUp JS -->
+        <script src="assets/js/jquery.scrollUp.min.js"></script>
+		<!-- Niceselect JS -->
+		<script src="assets/js/niceselect.js"></script>
+		<!-- Tilt Jquery JS -->
+		<script src="assets/js/tilt.jquery.min.js"></script>
+		<!-- Owl Carousel JS -->
+        <script src="assets/js/owl-carousel.js"></script>
+		<!-- counterup JS -->
+		<script src="assets/js/jquery.counterup.min.js"></script>
+		<!-- Steller JS -->
+		<script src="assets/js/steller.js"></script>
+		<!-- Wow JS -->
+		<script src="assets/js/wow.min.js"></script>
+		<!-- Magnific Popup JS -->
+		<script src="assets/js/jquery.magnific-popup.min.js"></script>
+		<!-- Counter Up CDN JS -->
+		<script src="http://cdnjs.cloudflare.com/ajax/libs/waypoints/2.0.3/waypoints.min.js"></script>
+		<!-- Bootstrap JS -->
+		<script src="assets/js/bootstrap.min.js"></script>
+		<!-- Main JS -->
+		<script src="assets/js/main.js"></script>
+    </body>
+</html>
--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,10 +1,67 @@
- <!-- Header-->
- <header class="header-image text-white">
-            <div class="container px-4 text-center">
-                <h3 class="fw-bolder text-light">Bienvenu(e) sur Rdv_Medical </h3>
-                <p class="text-dark" >Grace a cette application vous pouvez prendre rendez-vous avec des medecins professionnels</p>
-                <p class="text-dark"> depuis la maison alors a toi de voir la prochaine date de rendez-vous!!</p>
-                <a class="btn btn-success" href="#">Prendre rendez-vous</a>
-            </div>
+<header class="header" >
+			<!-- Debut Topbar -->
+			<div class="topbar">
+				<div class="container">
+					<div class="row">
+						<div class="col-lg-6 col-md-5 col-12">
+							<!-- Debut Contact -->
+							<ul class="top-link">
+								<li><a href="#">Doctors</a></li>
+								<li><a href="#">Contact</a></li>
+							</ul>
+							<!-- Fin Contact -->
+						</div>
+						<div class="col-lg-6 col-md-7 col-12">
+							<!-- Top Contact -->
+							<ul class="top-contact">
+								<li><i class="fa fa-phone"></i>+221 33 802 50 96</li>
+								<li><i class="fa fa-envelope"></i><a href="mailto:yannnzaba@gmail.com">himalayasommet@gmail.com</a></li>
+							</ul>
+							<!-- Fin Top Contact -->
+						</div>
+					</div>
+				</div>
+			</div>
+			<!-- Fin Topbar -->
+			<!-- Debut Header Inner -->
+			<div class="header-inner">
+				<div class="container">
+					<div class="inner">
+						<div class="row">
+							<div class="col-lg-3 col-md-3 col-12">
+								<!-- Debut Logo -->
+								<div class="logo">
+									<a href="index.html"><img src="assets/img/logo.png" alt="#"></a>
+								</div>
+								<!-- Fin Logo -->
+								<!-- Debut Mobile Nav -->
+								<div class="mobile-nav"></div>
+								<!-- Fin Mobile Nav -->
+							</div>
+							<div class="col-lg-7 col-md-9 col-12">
+								<!-- Debut Main Menu -->
+								<div class="main-menu">
+									<nav class="navigation">
+										<ul class="nav menu">
+											
+											<li class="active"><a href="#">Accueil</a></li>
+											<li><a href="#">  Medecins </a></li>
+											<li><a href="contact.html"> Contact</a></li>
+										</ul>
+									</nav>
+								</div>
+								<!--/ Fin Main Menu -->
+							</div>
+							<div class="col-lg-2 col-12">
+								<div class="get-quote">
+									<a href="#" class="btn btn-primary">êtes-vous medecin?</a>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+			<!--/ Fin Header  -->
+		</header>
       
       
